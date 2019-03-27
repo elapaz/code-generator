d2e478ab67cfb4c7c4b0749fb8087954749ebc07
Eduardo <tke2005@gmail.com>
2019-03-27 20:29:03 -0300

    Bugfixes

--- generate.py
+++ generate.py
@@ -18,12 +18,16 @@ def load_data(file):
               help='Data input', default='data/config.yml')
 @click.option('--template', type=click.STRING,
               help='Name of the template to apply', default='actions.js')
+@click.option('-o', '--output', help='Output file name', default='')
 @click.option('-v', '--verbose', count=True, help='Increment verbosity')
-def code_generator_app(data, template, verbose):
+def code_generator_app(data, template, output, verbose):
     templateLoader = jinja2.FileSystemLoader(searchpath="templates/")
     templateEnv = jinja2.Environment(loader=templateLoader)
-    if not os.path.isfile(data) or not os.path.isfile(template):
-        logger.error('Data or template invalid')
+    if not os.path.isfile(data):
+        logger.error('Data argument invalid')
+        return
+    if not os.path.isfile('templates/{}'.format(template)):
+        logger.error('Template argument invalid')
         return
 
     template_instance = templateEnv.get_template(template)
@@ -34,7 +38,10 @@ def code_generator_app(data, template, verbose):
     if not os.path.exists('output'):
         os.makedirs('output')
 
-    with open('output/{0}'.format(template), 'w') as wf:
+    if not output:
+        output = template
+
+    with open('output/{0}'.format(output), 'w') as wf:
         wf.write(rendered_content)
 
 
